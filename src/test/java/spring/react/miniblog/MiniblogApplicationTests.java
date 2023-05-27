896c78307945f9b033bb176828d0ac647f3982d1
ondjj <jcjk0302@gmail.com>
2023-05-27 19:45:05 +0900

    리액트 - 스프링 - 미니 블로그 프로젝트

---
+++ src/test/java/spring/react/miniblog/MiniblogApplicationTests.java
@@ -0,0 +1,13 @@
+package spring.react.miniblog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiniblogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
